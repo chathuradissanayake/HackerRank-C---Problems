56f4f2acfddf3369e9f1213b74fcaf5b016b1e91
Chathura Dissanayake <chathurakosala6@gmail.com>
2024-04-27 13:47:57 +0530

    first commit

---
+++ Q01 Say Hello, World! With C++.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdio>
+
+//https://www.hackerrank.com/challenges/cpp-hello-world/problem
+
+using namespace std;
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
