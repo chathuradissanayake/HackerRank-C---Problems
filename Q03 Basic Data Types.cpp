fcc96ef862414f43a8a5f3a1a77e9cdc0bdf4998
Chathura Dissanayake <chathurakosala6@gmail.com>
2024-05-08 01:17:45 +0530

    Java

---
+++ Q03 Basic Data Types.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+//https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/problem
+
+int main() {
+    // Complete the code.
+        int a;
+    long b;
+    char c;
+    float d; 
+    double e;
+    scanf("%d %ld %c %f %lf",&a, &b,&c,&d,&e);
+    printf("%d \n",a);
+    printf("%ld \n",b);
+    printf("%c \n",c);
+    printf("%f \n",d);
+    printf("%lf \n",e);
+
+
+    
+    return 0;
+}
